e6873b019d7e7dd09c3d8c764cefb1408b7a3db2
mehdi el kaddouri <mehdielkaddouri@MacBook-Pro-de-ippo.local>
2020-10-19 18:50:07 +0200

    1er push structure du projet effectuer

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss';
+import Sidebar from './Sidebar/SideBar'
+import Chat from './Chat/Chat'
+
+function App() {
+  return (
+    <div>
+       <Sidebar/>
+       <Chat/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDEqxc0VIxPMF97xWlmTZTelHaLxRE5C0E",
+    authDomain: "whatsapp-e3380.firebaseapp.com",
+    databaseURL: "https://whatsapp-e3380.firebaseio.com",
+    projectId: "whatsapp-e3380",
+    storageBucket: "whatsapp-e3380.appspot.com",
+    messagingSenderId: "452292869273",
+    appId: "1:452292869273:web:667d2423f54450f3cf4c08"
+};
